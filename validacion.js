cb6cde9ae20a01a521dae3d2d2c2bec272247cf7
Isabella <isabellachiaradia@hotmail.com>
2023-08-11 18:27:55 -0300

    feat: add functions for the form validation

--- validacion.js
+++ validacion.js
@@ -6,4 +6,73 @@ document.addEventListener("DOMContentLoaded", function() {
     const mensajeTextarea = form.querySelector('textarea[name="mensaje"]');
     const submitButton = form.querySelector(".formcontato__botao");
 
+    form.addEventListener("submit", function(event) {
+        if (!nombreInput.value.trim()) {
+            event.preventDefault();
+            showErrorMessage(nombreInput, "Campo Nombre no debe estar vacío.");
+        } else if (nombreInput.value.length > 50) {
+            event.preventDefault();
+            showErrorMessage(nombreInput, "El nombre debe tener máximo 50 caracteres.");
+        }
+
+        if (!isValidEmail(emailInput.value.trim())) {
+            event.preventDefault();
+            showErrorMessage(emailInput, "Ingresa un correo electrónico válido.");
+        }
+
+        if (!asuntoInput.value.trim()) {
+            event.preventDefault();
+            showErrorMessage(asuntoInput, "Campo Asunto no debe estar vacío.");
+        } else if (asuntoInput.value.length > 50) {
+            event.preventDefault();
+            showErrorMessage(asuntoInput, "El asunto debe tener máximo 50 caracteres.");
+        }
+
+        if (!mensajeTextarea.value.trim()) {
+            event.preventDefault();
+            showErrorMessage(mensajeTextarea, "Campo Mensaje no debe estar vacío.");
+        } else if (mensajeTextarea.value.length > 300) {
+            event.preventDefault();
+            showErrorMessage(mensajeTextarea, "El mensaje debe tener máximo 300 caracteres.");
+        }
+    });
+
+    function showErrorMessage(inputElement, errorMessage) {
+        const errorElement = document.createElement("span");
+        errorElement.classList.add("formcontato__error");
+        errorElement.textContent = errorMessage;
+
+        const parent = inputElement.parentElement;
+        const existingError = parent.querySelector(".formcontato__error");
+        if (existingError) {
+            parent.removeChild(existingError);
+        }
+
+        parent.appendChild(errorElement);
+    }
+
+    function isValidEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+
+    [nombreInput, emailInput, asuntoInput, mensajeTextarea].forEach(function(input) {
+        input.addEventListener("input", function() {
+            const parent = input.parentElement;
+            const existingError = parent.querySelector(".formcontato__error");
+            if (existingError) {
+                parent.removeChild(existingError);
+            }
+
+            checkFormValidity();
+        });
+    });
+
+    function checkFormValidity() {
+        if (form.checkValidity()) {
+            submitButton.removeAttribute("disabled");
+        } else {
+            submitButton.setAttribute("disabled", "disabled");
+        }
+    }
 });
