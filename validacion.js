cac5f57f4763e17e6e30af48fc3fd5d178b3e083
Isabella <isabellachiaradia@hotmail.com>
2023-08-11 11:48:41 -0300

    feat: add validacion.js file to be used for the form validation

---
+++ validacion.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".formcontato__form");
+    const nombreInput = form.querySelector('input[name="nombre"]');
+    const emailInput = form.querySelector('input[name="email"]');
+    const asuntoInput = form.querySelector('input[name="asunto"]');
+    const mensajeTextarea = form.querySelector('textarea[name="mensaje"]');
+    const submitButton = form.querySelector(".formcontato__botao");
+
+});
